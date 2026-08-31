949b26bafa9108c2b336b9ebd6d0cc7e1153567c
capopaul <paul.capgras@utah.edu>
2026-08-31 10:21:57 -0600

    feat: update script such that i2c works

--- software/esp32_i2c/main/main.c
+++ software/esp32_i2c/main/main.c
@@ -32,63 +32,63 @@
 // Include i2c tools example
 #include "cmd_i2ctools.h"
 
-#define AUDIO_CODEC_RESETN_GPIO 32
-#define AUDIO_CODEC_RESETN_MASK (1ULL << AUDIO_CODEC_RESETN_GPIO)
+#define IO_EXPANDER_RESETN_GPIO 32
+#define AUDIO_CODEC_RESETN_GPIO 33
 
 static const char *TAG = "i2c-tools";
 
-static gpio_num_t i2c_gpio_sda = 16;
-static gpio_num_t i2c_gpio_scl = 17;
+static gpio_num_t i2c_gpio_sda = 19;
+static gpio_num_t i2c_gpio_scl = 21;
 
 static i2c_port_t i2c_port = I2C_NUM_0;
 
-void reset_audio_codec(void)
+void release_reset(gpio_num_t gpio)
 {
-    // Configure GPIO as output
+    ESP_ERROR_CHECK(gpio_set_level(gpio, 1));
+}
+
+void hold_reset_low(gpio_num_t gpio)
+{
     gpio_config_t io_conf = {
-        .pin_bit_mask = AUDIO_CODEC_RESETN_MASK,
+        .pin_bit_mask = (1ULL << gpio),
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE};
-    gpio_config(&io_conf);
+        .intr_type = GPIO_INTR_DISABLE
+    };
 
-    // Pull reset pin LOW
-    gpio_set_level(AUDIO_CODEC_RESETN_GPIO, 0);
-    printf("Audio codec reset pin LOW\n");
-
-    // Wait 1 millisecond
-    vTaskDelay(pdMS_TO_TICKS(1000));
-
-    // Pull reset pin HIGH
-    gpio_set_level(AUDIO_CODEC_RESETN_GPIO, 1);
-    printf("Audio codec reset pin HIGH\n");
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+    ESP_ERROR_CHECK(gpio_set_level(gpio, 0));
 }
 
 void app_main(void)
 {
     printf("Hello world!\n");
 
-    reset_audio_codec();
+    hold_reset_low(IO_EXPANDER_RESETN_GPIO);
+    hold_reset_low(AUDIO_CODEC_RESETN_GPIO);
 
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    // Create UART console
     esp_console_repl_t *repl = NULL;
     esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
 
     esp_console_dev_uart_config_t uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_console_new_repl_uart(&uart_config, &repl_config, &repl));
 
+    // Create I2C bus
     i2c_master_bus_config_t i2c_bus_config = {
         .clk_source = I2C_CLK_SRC_DEFAULT,
         .i2c_port = i2c_port,
         .scl_io_num = i2c_gpio_scl,
         .sda_io_num = i2c_gpio_sda,
         .glitch_ignore_cnt = 7,
-        .flags.enable_internal_pullup = true,
+        .flags.enable_internal_pullup = false,
     };
 
     ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_config, &tool_bus_handle));
 
-    register_i2ctools();
 
     printf("\n ==============================================================\n");
     printf(" |             Steps to Use i2c-tools                         |\n");
@@ -102,6 +102,9 @@ void app_main(void)
     printf(" |                                                            |\n");
     printf(" ==============================================================\n\n");
 
-    // start console REPL
+    // Register commands
+    register_i2ctools();
+
+    // Start interactive console
     ESP_ERROR_CHECK(esp_console_start_repl(repl));
 }
